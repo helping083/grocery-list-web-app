d3746fb66d37e22dc4972885addc3f909b500fd1
helping083 <helping083@gmail.com>
2020-11-02 16:12:24 +0200

    added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+      jest: true,
+    },
+    extends: ["plugin:react/recommended", "airbnb", "plugin:prettier/recommended"],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+      "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+      "no-underscore-dangle": [0, { allow: [] }],
+      "no-nested-ternary": "off",
+      "react/require-default-props": [0],
+      "react/jsx-one-expression-per-line": "off",
+      "import/prefer-default-export": "off",
+    },
+};
